4ee63c8a26061339b594951d8e88d3463db60873
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2025-12-20 23:07:50 +0530

    jest test

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import sum from './sum.js';
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get('/', (req, res) => {
+  res.json({msg:'Hello, World!'});
+});
+
+app.get("/getSum/:a/:b", (req, res) => {
+  const { a, b } = req.params;
+  const sumResult = sum(parseFloat(a), parseFloat(b));
+  res.json({ msg: sumResult });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on Listen ${PORT}`);
+});
---
+++ sum.test.js
@@ -0,0 +1,11 @@
+import sum from './sum.js';
+
+test('adds 2 + 2 to equal 4', () => {
+  expect(sum(2, 2)).toBe(4);
+});
+test('adds -1 + 1 to equal 0', () => {
+  expect(sum(-1, 1)).toBe(0);
+});
+test('adds 0 + 0 to equal 0', () => {
+  expect(sum(0, 0)).toBe(0);
+});
